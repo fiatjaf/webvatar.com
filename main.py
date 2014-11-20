4146f31d8a6a25618bec9db74e205485b4e74f0d
fiatjaf@spooner <fiatjaf@gmail.com>
2014-11-20 07:51:49 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# -*- encoding: utf-8 -*-
+
+import os
+import urlparse
+import requests
+import microdata
+from mf2py.parser import Parser
+from flask import Flask, session, jsonify, redirect, request, render_template, abort, redirect
+
+import redis as r
+url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+redis = r.Redis(host=url.hostname, port=url.port, password=url.password)
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/<addr>/')
+def avatar(addr):
+    if not addr.startswith('https://') and not addr.startswith('http://'):
+        addr = 'http://' + addr
+    addr = urlparse.urlparse(addr)
+    host = addr.netloc
+    path = addr.path if addr.path and addr.path != '/' else ''
+
+    # search in our cache
+    cached_src = redis.get(host)
+    if not cached_src and path:
+        cached_src = redis.get(host + path)
+    if cached_src:
+        return redirect(cached_src)
+
+    else:
+        # otherwise fetch it from the live page
+        protocol = addr.scheme
+
+        # try microformats2
+        src = None
+        base = protocol + '://' + host + path
+        try:
+            parsed = Parser(url=base)
+            for item in parsed.to_dict()['items']:
+                if u'h-card' in item['type']:
+                    if u'photo' in item['properties']:
+                        src = item['properties']['photo'][0]
+                        break
+        except requests.exceptions.ConnectionError:
+            pass
+
+        # try microdata
+        if not src:
+            try:
+                html = requests.get(base).text.encode('utf-8')
+                items = microdata.get_items(html)
+                if len(items):
+                    image = items[0].image
+                    if image:
+                        src = image if image.startswith('http://') or image.startswith('https://') else urlparse.urljoin(base, image)
+            except requests.exceptions.ConnectionError:
+                pass
+
+        # when we find nothing
+        if not src:
+            return abort(404)
+
+        # save to redis
+        redis.set(host + path, src)
+
+        # return
+        return redirect(src)
