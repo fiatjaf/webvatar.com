8c752f5c6bec37b136f2e483b4d6a76b4e869c67
fiatjaf@spooner <fiatjaf@gmail.com>
2014-12-23 22:23:36 -0500

    working version, very crude

--- web.go
+++ web.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/PuerkitoBio/goquery"
 	"github.com/go-martini/martini"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -15,8 +18,43 @@ func main() {
 	})
 
 	m.Get("/**", func(params martini.Params, res http.ResponseWriter, req *http.Request) {
-		url := strings.ToLower(params["_1"])
-		http.Redirect(res, req, url, 302)
+
+		// parse url
+		target := strings.ToLower(params["_1"])
+		if strings.HasPrefix(target, "http://") == false || strings.HasPrefix(target, "https://") {
+			target = "http://" + target
+		}
+		t, err := url.Parse(target)
+		if err != nil {
+			log.Fatal(err)
+			http.Error(res, "Oops!", http.StatusInternalServerError)
+			return
+		}
+
+		// parse HTML
+		doc, err := goquery.NewDocument(t.String())
+		if err != nil {
+			log.Fatal(err)
+			http.Error(res, "Oops!", http.StatusInternalServerError)
+			return
+		}
+		imageUrl := ""
+		doc.Find("h-card u-photo, [rel=\"icon\"]").Each(func(i int, s *goquery.Selection) {
+			imageHref, found := s.Attr("href")
+			if found == false {
+				return
+			}
+			uImageHref, err := url.Parse(imageHref)
+			if err != nil {
+				log.Fatal(err)
+				http.Error(res, "Oops!", http.StatusInternalServerError)
+				return
+			}
+			imageUrl = t.ResolveReference(uImageHref).String()
+			return
+		})
+
+		http.Redirect(res, req, imageUrl, 302)
 	})
 
 	port := os.Getenv("PORT")
